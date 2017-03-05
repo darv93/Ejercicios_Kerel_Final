08f7ebb5bcb0ecf7552988950dcbd8f475ed5117
Diego <Diego@Diego-PC>
2017-03-04 23:30:08 -0500

    Kerel

--- src/Ejercicio5_1/Ladrillos.java
+++ src/Ejercicio5_1/Ladrillos.java
@@ -16,10 +16,11 @@ public class Ladrillos {
     private Thing balon;
     private Wall pared;
     private boolean a[][];
-    public Ladrillos(){
-        this.a=new boolean [5][5];
-        this.unal=new City(10,13);
-        this.joe=new Robot(unal,9,1,Direction.NORTH);
+    
+public Ladrillos(){
+      this.a=new boolean [5][5];
+      this.unal=new City(10,13);
+      this.joe=new Robot(unal,9,1,Direction.NORTH);
         for(int i=0;i<=4;i++){
             for(int j=0;j<=4;j++){
                 if(i==0 || i==4){
@@ -35,7 +36,9 @@ public class Ladrillos {
             }
         }
     }
-    public void mover(){
+
+
+    /*public void mover(){
         boolean b[][]=new boolean [5][5];
         joe.move();
         for(int i=0;i<=4;i++){
@@ -106,4 +109,115 @@ public class Ladrillos {
             
         }
     }
+*/
+
+public void mover(){
+    
+    int x = 0; //Para manejar las filas
+      int y = 0; //Para manejar las columnas
+      int flag = 3;  //Bandera para saber que recorrido se debe realizar
+      int n = 0; //Contador de elementos
+      
+      //Total de elementos en la matriz
+      int elementos = this.a.length * this.a[0].length;
+ 
+      // Recorrido en espiral
+      //System.out.println("\nMostrando recorrido en espiral");
+      while (n < elementos) {
+         //System.out.println("\nFlag : " + flag);
+         switch (flag) {
+         case 0: // Recorrido de izquierda a derecha
+             
+             this.joe.move();
+             for (int m = x; m <= this.a[0].length - y-1; m++) {
+               System.out.print(" " + this.a[x][m]);
+               if(this.joe.canPickThing()){
+                   this.joe.pickThing();
+                   this.a[x][m]=true;
+               }
+               this.joe.move();
+               n++;
+               
+            }
+                        
+                     
+            flag++;
+            for (int i=0;i<3;i++){
+                 this.joe.turnLeft();
+             }
+            break;
+ 
+            case 1: //Recorrido de arriba abajo
+         
+             
+             this.joe.move();        
+            for (int m = x + 1; m < this.a.length - x+1 ; m++) {
+               System.out.print(" " + this.a[m][this.a[0].length - 1 - y]);
+               if(this.joe.canPickThing()){
+                   this.joe.pickThing();
+                   this.a[m][this.a[0].length - 1 - y]=true;
+               }
+               this.joe.move();
+               
+               n++;
+               
+            }
+                      
+            flag++;
+            for (int i=0;i<3;i++){
+                 this.joe.turnLeft();
+             }
+            break;
+ 
+         case 2: //Recorrido de derecha a izquierda
+             
+             
+             this.joe.move();
+            for (int m = this.a[0].length - 1 - y; m >= y; m--) {
+               System.out.print(" " + this.a[this.a.length - 1 - x][m]);
+               if(this.joe.canPickThing()){
+                   this.joe.pickThing();
+                   this.a[this.a.length - 1 - x][m]=true;
+               }
+               this.joe.move();
+               n++;
+               
+            }
+                     
+            flag++;
+            for (int i=0;i<3;i++){
+                 this.joe.turnLeft();
+             }
+            break;
+ 
+         case 3: //Recorrido de abajo a arriba
+             
+             
+             this.joe.move();
+            for (int m = this.a.length - 1 - x; m >= x + 1; m--) {
+               System.out.print(" " + this.a[m][y]);
+               if(this.joe.canPickThing()){
+                   this.joe.pickThing();
+                   this.a[m][y]=true;
+               }
+               this.joe.move();
+               n++;
+               
+            }
+                
+             
+             
+            flag = 0;
+            //Se aumentan las filas y las columnas para un subnivel en el espiral
+            x++;
+            y++;
+            for (int i=0;i<3;i++){
+                 this.joe.turnLeft();
+             }
+            break;
+         } 
+ 
+    }
+      this.joe.move();
+}
 }
---
+++ src/RecorrerMatrizEspiral/RecorridoEspiral.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RecorrerMatrizEspiral;
+
+/**
+ *
+ * @author Diego
+ */
+public class RecorridoEspiral {
+   public static void main(String[] args) {
+      // Declaracion del arreglo bidimensional : Cuadrado
+      int[][] matriz = new int[10][10];
+ 
+      // Lleno el arreglo
+      for (int i = 0; i < matriz.length; i++) {
+         for (int m = 0; m < matriz[0].length; m++) {
+            matriz[i][m] = (int) (Math.random() * 100);
+         }
+      }
+ 
+      // Lleno el arreglo
+      System.out.println("Mostrando matriz : ");
+      for (int i = 0; i < matriz.length; i++) {
+         for (int m = 0; m < matriz[0].length; m++) {
+            System.out.print(" " + matriz[i][m]);
+         }
+         System.out.println("");
+      }
+ 
+      int x = 0; //Para manejar las filas
+      int y = 0; //Para manejar las columnas
+      int flag = 0;  //Bandera para saber que recorrido se debe realizar
+      int n = 0; //Contador de elementos
+      //Total de elementos en la matriz
+      int elementos = matriz.length * matriz[0].length;
+ 
+      // Recorrido en espiral
+      System.out.println("\nMostrando recorrido en espiral");
+      while (n < elementos) {
+         //System.out.println("\nFlag : " + flag);
+         switch (flag) {
+         case 0: // Recorrido de izquierda a derecha
+            for (int m = x; m < matriz[0].length - y; m++) {
+               System.out.print(" " + matriz[x][m]);
+               n++;
+            }
+            flag++;
+            break;
+ 
+         case 1: //Recorrido de arriba abajo
+            for (int m = x + 1; m < matriz.length - x; m++) {
+               System.out.print(" " + matriz[m][matriz[0].length - 1 - y]);
+               n++;
+            }
+            flag++;
+            break;
+ 
+         case 2: //Recorrido de derecha a izquierda
+            for (int m = matriz[0].length - 2 - y; m >= y; m--) {
+               System.out.print(" " + matriz[matriz.length - 1 - x][m]);
+               n++;
+            }
+            flag++;
+            break;
+ 
+         case 3: //Recorrido de abajo a arriba
+            for (int m = matriz.length - 2 - x; m >= x + 1; m--) {
+               System.out.print(" " + matriz[m][y]);
+               n++;
+            }
+            flag = 0;
+            //Se aumentan las filas y las columnas para un subnivel en el espiral
+            x++;
+            y++;
+            break;
+         } 
+ 
+      }
+ 
+   }
+}
